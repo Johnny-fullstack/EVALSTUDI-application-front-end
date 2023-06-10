1f845dd4250cf132154363878cba4932f440bfc7
unknown <paul.bellenger@yahoo.fr>
2023-06-10 17:09:21 +0200

    traitement du formulaire

---
+++ front/form.js
@@ -0,0 +1,41 @@
+// Sélection du formulaire
+const form = document.querySelector('form');
+
+// Écoute de l'événement de soumission du formulaire
+form.addEventListener('submit', function(event) {
+  event.preventDefault(); // Empêche l'envoi du formulaire par défaut
+
+  // Récupération des valeurs des champs
+  const objetD = document.getElementById('objetD').value;
+  const nom = document.getElementById('nom').value;
+  const prenom = document.getElementById('prénom').value;
+  const email = document.getElementById('email').value;
+  const message = document.getElementById('message').value;
+
+  // Construction du corps du message
+  var emailBody = 'Objet : ' + objetD + '\n\n';
+  emailBody += 'Message : ' + message + '\n\n';
+  emailBody += 'Nom : ' + nom + '\n';
+  emailBody += 'Prénom : ' + prenom;
+  emailBody += 'email : ' + email;
+
+  sendEmail(emailBody);
+
+  function sendEmail(body) {
+    // Remplacez l'adresse e-mail fictive par votre adresse e-mail de destination
+    var recipientEmail = 'johnny.kavu@gmail.com';
+    
+    // Envoi de l'e-mail fictif en utilisant l'API mailto
+    var mailtoLink = 'mailto:' + recipientEmail + '&body=' + encodeURIComponent(body);
+
+    window.open(mailtoLink)
+    console.log(emailBody);
+    afficherConfirmation();
+  }
+  
+  // Fonction pour afficher le message de confirmation
+function afficherConfirmation() {
+    alert("Merci pour votre contribution Mr. ou Mme."+ nom + "\n" + "Votre message a été transmis avec succès.");
+  }
+
+});
