ff3524b1cb880c450778dc76c6bad30e8924569f
unknown <paul.bellenger@yahoo.fr>
2023-06-12 20:14:20 +0200

    validation du formulaire

--- front/form.js
+++ front/form.js
@@ -1,34 +1,3 @@
-// Fonction de validation
-function validateField(champId, regex, errorMessage) {
-  var champ = document.getElementById(champId);
-  var champValue = champ.value;
-
-  if (regex.test(champValue)) {
-      alert(errorMessage);
-      champ.focus();
-      return false;
-  }
-
-  return true;
-}
-
-// Vérification des champs à la perte du focus
-document.getElementById('nom').addEventListener('blur', function() {
-  validateField('nom', /<script.*?>.*?<\/script>/gi, "Veuillez entrer un nom valide");
-});
-
-document.getElementById('prénom').addEventListener('blur', function() {
-  validateField('prénom', /<script.*?>.*?<\/script>/gi, "Veuillez entrer un prénom valide");
-});
-
-document.getElementById('objetD').addEventListener('blur', function() {
-  validateField('objetD', /<script.*?>.*?<\/script>/gi, "Veuillez entrer un objet valide");
-});
-
-document.getElementById('message').addEventListener('blur', function() {
-  validateField('message', /<script.*?>.*?<\/script>/gi, "Veuillez entrer un message valide");
-});
-
 const form = document.querySelector('form');
 
 form.addEventListener('submit', function(event) {
@@ -40,14 +9,6 @@ form.addEventListener('submit', function(event) {
   const prenom = document.getElementById('prénom').value;
   const email = document.getElementById('email').value;
   const message = document.getElementById('message').value;
-
-  // Revérification des champs
-  if (!validateField('objetD', /<script.*?>.*?<\/script>/gi, "Veuillez entrer un objet valide") ||
-      !validateField('nom', /<script.*?>.*?<\/script>/gi, "Veuillez entrer un nom valide") ||
-      !validateField('prénom', /<script.*?>.*?<\/script>/gi, "Veuillez entrer un prénom valide") ||
-      !validateField('message', /<script.*?>.*?<\/script>/gi, "Veuillez entrer un message valide")) {
-    return;
-  }
 
   // Construction du corps du message
   var emailBody = 'Objet : ' + objetD + '\n\n';
